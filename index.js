6ffbcc11646b3d03ad272810c40c3d1f6248e80a
Andreas Pizsa <hopster@gmail.com>
2014-10-25 10:50:32 +0200

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * kickstart-app <https://github.com/AndreasPizsa/kickstart-app>
+ *
+ * Copyright (c) 2014 Andreas Pizsa, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
